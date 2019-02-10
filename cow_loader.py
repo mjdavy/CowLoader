35038482e93ed9ae43e2850cd1b7a7e1cf41cf6f
Martin Davy <mjdavy@hotmail.com>
2019-02-10 11:38:54 -0500

    now downloads all cow pics to output_folder

--- cow_loader.py
+++ cow_loader.py
@@ -2,8 +2,14 @@ from pathlib import Path
 from google_images_download import google_images_download
 
 
-cow_breeds = ['Fresian Cow','Shorthorn Cow','Vosges Cow','Jersey Cow','Allgauer Cow','Guernsey Cow','Angler Cow','Charolais Cow','Brown Swiss Cow']
-output_folder = Path('D:\Data\Cows')
+cow_breeds = ['Fresian','Shorthorn','Vosges','Jersey','Allgauer','Guernsey','Angler','Charolais','Brown Swiss']
+output_folder = Path('D:\\Data\\Cows')
 
-print(cow_breeds)
-print(output_folder)
+arguments = [{"keywords":f"{k} Cow","output_directory":f"{output_folder}"} for k in cow_breeds]
+
+from google_images_download import google_images_download
+
+response = google_images_download.googleimagesdownload()
+
+for arg in arguments:
+    paths = response.download(arg)
