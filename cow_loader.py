052b54445fe8e442485cef4ce48434a1ec51a392
Martin Davy <mjdavy@hotmail.com>
2019-02-10 08:33:08 -0500

    Initial version

---
+++ cow_loader.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from google_images_download import google_images_download
+
+
+cow_breeds = ['Fresian Cow','Shorthorn Cow','Vosges Cow','Jersey Cow','Allgauer Cow','Guernsey Cow','Angler Cow','Charolais Cow','Brown Swiss Cow']
+output_folder = Path('D:\Data\Cows')
+
+print(cow_breeds)
+print(output_folder)
